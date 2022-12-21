e954305d618e12796131cc4f5a81001923ed94dd
Hakoyu <Hakoyu@foxmail.com>
2022-12-21 23:11:51 +0800

    修复初始化时保存按钮会闪烁的bug

--- Tools/ModManager/ModManager.cs
+++ Tools/ModManager/ModManager.cs
@@ -66,6 +66,7 @@ namespace StarsectorTools.Tools.ModManager
             CheckUserData();
             RefreshModsContextMenu();
             RefreshCountOfListBoxItems();
+            ResetRemindSaveThread();
             GC.Collect();
         }
 
