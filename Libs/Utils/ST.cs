7d628bf767d3640a56e0009c69180876a9f4b02c
Hakoyu <Hakoyu@foxmail.com>
2023-03-07 18:13:56 +0800

    设置Assembly信息

--- Libs/Utils/ST.cs
+++ Libs/Utils/ST.cs
@@ -4,21 +4,21 @@
     public static class ST
     {
         /// <summary>核心文件夹</summary>
-        public static string CoreDirectory => "STCore";
+        public const string CoreDirectory = "STCore";
 
         /// <summary>拓展目录</summary>
-        public static string ExtensionDirectories => "STExtension";
+        public const string ExtensionDirectories = "STExtension";
 
         /// <summary>配置文件</summary>
-        public static string ConfigTomlFile => $"{CoreDirectory}\\Config.toml";
+        public const string ConfigTomlFile = $"{CoreDirectory}\\Config.toml";
 
         /// <summary>日志文件</summary>
-        public static string LogFile => $"{CoreDirectory}\\{nameof(StarsectorTools)}.log";
+        public const string LogFile = $"{CoreDirectory}\\{nameof(StarsectorTools)}.log";
 
         /// <summary>拓展信息文件</summary>
-        public static string ExtensionInfoFile => "Extension.toml";
+        public const string ExtensionInfoFile = "Extension.toml";
 
         /// <summary>游戏版本</summary>
-        public static string Version => "0.8.0.0";
+        public const string Version = "0.8.0.0";
     }
 }
